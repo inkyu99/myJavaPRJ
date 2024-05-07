a4c15e3dae88444bad3ea3e3b1e1c9001000c086
inkyu99 <inkyu990730@naver.com>
2024-05-07 15:26:23 +0900

    Code07_12

---
+++ src/main/java/Code07_12.java
@@ -0,0 +1,18 @@
+class Rabbit4 {
+    private String shape;
+    private int xPos;
+    private int yPos;
+
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+
+}
+public class Code07_12 {
+    public static void main(String[] args) {
+        Rabbit4 rabbit = new Rabbit4();
+
+        rabbit.setPosition(100,200);
+    }
+}
