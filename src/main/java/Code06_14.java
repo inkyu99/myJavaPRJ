ca13a95f9932e0ea6cba7fb06af6a78d2e798440
inkyu99 <inkyu990730@naver.com>
2024-05-07 15:00:08 +0900

    Code06_14

--- src/main/java/Code06_14.java
+++ src/main/java/Code06_14.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class code06_14 {
+public class Code06_14 {
     public static void main(String[] args){
         Scanner s =new Scanner(System.in);
         int hap = 0;
