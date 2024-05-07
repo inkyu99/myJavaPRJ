02f7d1cf064d65b71ab8fcbae10cd3c18f1a5e69
inkyu99 <inkyu990730@naver.com>
2024-05-07 14:58:32 +0900

    Code05_07

--- src/main/java/Code05_07.java
+++ src/main/java/Code05_07.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class code05_07 {
+public class Code05_07 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int score;
