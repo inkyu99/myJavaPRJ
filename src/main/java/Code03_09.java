ba274607901212a30d518cbdd6ee354c34746ec0
inkyu99 <inkyu990730@naver.com>
2024-05-07 14:46:17 +0900

    Code03_09

--- src/main/java/Code03_09.java
+++ src/main/java/Code03_09.java
@@ -1,5 +1,5 @@
-public class code03_09 {
-    public static void main(String[] args){
+public class Code03_09 {
+    public static void main(String[] args) {
         int num = 20;
         System.out.print(++num + " ");
         System.out.print(num++ + " ");
