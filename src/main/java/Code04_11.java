ece67bc24d732a0d459e01b60896c9affbc86cdb
inkyu99 <inkyu990730@naver.com>
2024-05-07 14:47:14 +0900

    Code04_11

--- src/main/java/Code04_11.java
+++ src/main/java/Code04_11.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class code04_11 {
+public class Code04_11 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String var1, var2;
