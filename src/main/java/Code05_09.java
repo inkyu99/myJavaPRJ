9057864b3d6e857d4a6035657737e1f36b0884cb
inkyu99 <inkyu990730@naver.com>
2024-05-07 14:58:49 +0900

    Code05_09

--- src/main/java/Code05_09.java
+++ src/main/java/Code05_09.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class code05_09 {
+public class Code05_09 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int select;
