de810a47622461c4ef25dce5d2cfce6918035a9e
inkyu99 <inkyu990730@naver.com>
2024-05-13 14:02:35 +0900

    Code08_01

---
+++ src/main/java/Code08_01.java
@@ -0,0 +1,23 @@
+class Rabbit5 {
+    String shape;
+    int xPos;
+    int yPos;
+    static int count;
+
+    Rabbit5() {
+        count++;
+    }
+}
+public class Code08_01 {
+    public static void main(String[] args){
+        System.out.println("객체 생성 전의 총 토끼 수 ==>"+ Rabbit5.count);
+
+        Rabbit rabbit1 = new Rabbit();
+        System.out.println("토끼 객체1 생성 후의 총 토끼 수 ==>"+ Rabbit5.count);
+
+        Rabbit rabbit2 = new Rabbit();
+        System.out.println("토끼 객체2 생성 후의 총 토끼 수 ==>" + Rabbit5.count);
+    }
+
+
+}
